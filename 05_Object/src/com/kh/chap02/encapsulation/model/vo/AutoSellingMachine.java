97f52949f3e453964459bc6c7a2061c33bb0f50a
u1000e <73210038+u1000e@users.noreply.github.com>
2026-03-19 17:52:39 +0900

    feat : 19일 수업내용

---
+++ 05_Object/src/com/kh/chap02/encapsulation/model/vo/AutoSellingMachine.java
@@ -0,0 +1,28 @@
+package com.kh.chap02.encapsulation.model.vo;
+
+
+// 필드부, 생성자부, 메소드부 
+public class AutoSellingMachine {
+	// 필드(Field)부
+	/*
+	 * 값을 담는 부분
+	 * 
+	 * 필드 == 멤버변수
+	 * 
+	 * 객체들이 가져야할 공통적인 속성을 기술해 놓는 것
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
