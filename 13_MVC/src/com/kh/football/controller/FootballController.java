fbf4850988ddbf3c6704b7dfe7fe885440709b3f
u1000e <73210038+u1000e@users.noreply.github.com>
2026-04-03 17:50:34 +0900

    feat : Java수업 진도 끝!

--- 13_MVC/src/com/kh/football/controller/FootballController.java
+++ 13_MVC/src/com/kh/football/controller/FootballController.java
@@ -2,6 +2,7 @@ package com.kh.football.controller;
 
 import java.util.List;
 
+import com.kh.football.model.dto.FootballPlayerDto;
 import com.kh.football.model.service.FootballService;
 import com.kh.football.model.vo.FootballPlayer;
 
@@ -15,5 +16,39 @@ public class FootballController {
 		return list;
 	}
 	
+	// 뷰에서 축구선수를 추가하고 싶을 때 호출하는 메소드
+	public void addFootballPlayer(String name, 
+								  String position, 
+								  int backNumber) {
+		// 귀찮음 이슈
+		//fs.addFootballPlayer(name, position, backNumber);
+	}
+	
+	// 선택지 == 2개
+	// 1. Object
+	// 2. FootballPlayerDto
+	public FootballPlayer addFootballPlayer(FootballPlayerDto player) {
+		return fs.addFootballPlayer(player);
+	}
+	
+	public boolean idCheck(int id) {
+		return fs.idCheck(id);  
+	}
+	
+	public void updateFootballPlayer(int id, FootballPlayerDto player) {
+		fs.updateFootballPlayer(id, player);
+	}
+	
+	public String deleteFootballPlayer(int id) {
+		return fs.deleteFootballPlayer(id);
+	}
+	
+	public void outputFootballPlayer() {
+		fs.outputFootballPlayer();
+	}
+	
+	public List<FootballPlayer> findByKeyword(String keyword) {
+		return fs.findByKeyword(keyword);
+	}
 	
 }
